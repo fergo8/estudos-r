89c08f0ab6fd18a87587dc248fc9ba1d0f6458bb
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-02-20 18:13:52 -0300

    Aula 1 - comando if else (exemplo 2)

--- aula1-if-else.R
+++ aula1-if-else.R
@@ -9,3 +9,29 @@ c <- 3
 if(a != b & b != c & c != a){
     cat("É um triângulo escaleno")
 }
+
+# EXEMPLO 2
+
+# Dois objetos numéricos
+a <- 2
+b <- 5
+
+# Estrutura de condição
+if(a > b){
+    cat(a)
+}else{
+    cat(b)
+}
+
+# EXEMPLO 3
+
+# Função ifelse
+x <- -25
+ifelse(x >= 0, sqrt(x), "O número é negativo")
+
+
+# ===========
+# EXERCÍCIO
+# Informe se um número é par ou ímpar usando a estrutura de condição ifelse.
+y <- 8
+ifelse(y %% 2 == 0, "Número par", "Número ímpar")
